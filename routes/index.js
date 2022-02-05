0cb42323f1b83e4d6e5169f5dd8ba3d863e95482
adel <adel_last@hotmail.com>
2022-02-05 17:25:35 +0100

    players list route

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,26 @@ const sports = [
   { id: 2, name: "basketball" },
   { id: 3, name: "judo" },
 ];
+
+const players = [
+  { id: 1, name: "Habib" },
+  { id: 2, name: "Mohammed Ali" },
+  { id: 3, name: "Tyson" },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("welcome to express");
 });
 
-/* GET home page. */
+/* GET sports page. */
 router.get("/sports", function (req, res, next) {
   res.send(sports);
+});
+
+/* GET players page. */
+router.get("/players", function (req, res, next) {
+  res.send(players);
 });
 
 module.exports = router;
