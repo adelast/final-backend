c3060fc5b033337328e4feaf87c6c5c4b35d459c
adel <adel_last@hotmail.com>
2022-02-05 17:08:02 +0100

    add sports route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
+const sports = [
+  { id: 1, name: "football" },
+  { id: 2, name: "basketball" },
+  { id: 3, name: "judo" },
+];
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("welcome to express");
+});
+
+/* GET home page. */
+router.get("/sports", function (req, res, next) {
+  res.send(sports);
 });
 
 module.exports = router;
